096338413e020ab40e87408008ab37263cac2da8
guratjatianom <guratelek@gmail.com>
2024-06-18 16:13:04 +0700

    feat: menambahkan biodata js

---
+++ gurat.js
@@ -0,0 +1,21 @@
+const nama = "gurat"
+const umur = 18
+const hobi = ["mancing", "main game"]
+const biodata = [
+    {
+    nama: "gjt",
+    umur: 1,
+    hobi: ["futsal"]
+    },
+    {
+        nama: "p",
+        umur: 2,
+        hobi: ["gktau"]
+    },
+    {
+        nama: "itu",
+        umur: 100,
+        hobi: ["ada"]
+    }
+]
+console.log(biodata[0].nama)
